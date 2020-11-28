462d1f8b32a27c3440e3436525ec14cbbb2d244a
Tatiana <tanchaz@ya.ru>
2020-11-29 00:20:31 +0300

    fffff

--- hello.py
+++ hello.py
@@ -1,6 +1,4 @@
-def wsgi_application(environ, start_response):
-    status = '200 OK'
-    headers = [('Content-Type', 'text/plain')]
-    start_response(status, headers )
-    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
-    return body
+def app(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
+                  encoding="utf8")]
